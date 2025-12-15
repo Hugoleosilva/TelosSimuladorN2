3ea84a124dc0d3add0fe52b8898c8b61417d9c37
Hugoleosilva <hugollsilva.dev@gmail.com>
2025-12-15 17:42:01 -0300

    Ajuste no index

---
+++ desafio_manutencao_hugo_leo/assets/index.js
@@ -0,0 +1,15 @@
+<div class="botoes">
+            <button id="meuBotao" style="padding: 20px; background: red; color: white;" onclick="avisar()">
+                CLIQUE AQUI
+            </button>
+        </div>
+
+        <div id="rodape" style="background: #333; color: white; padding: 10px; text-align: center;">
+            <p>XPTO - Todos os direitos reservados</p>
+        </div>
+    </div>
+
+    <script>
+        function avisar() {
+            alert("Você clicou no botão!");
+        }
